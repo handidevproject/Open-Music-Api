a8372ae049f8136991e971116b32c47341eba2ef
Handi <handikadwiputradev@gmail.com>
2024-10-07 23:52:20 +0700

    restructure with hapi plugin

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+
+const init = async () => {
+  const notesService = new NotesService();
+  const server = Hapi.server({
+    port: 3000,
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  try {
+    await server.register({
+      plugin: notes,
+      options: {
+        service: notesService,
+      },
+    });
+  } catch (err) {
+    console.error("Error registering plugin:", err);
+  }
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
